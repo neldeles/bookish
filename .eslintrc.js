cf7e0f1bc35136c982f86f1b223cdb35204ce4ef
neldeles <neldeles@gmail.com>
2021-07-13 04:28:49 +0800

    make the first e2e test pass

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    'jest/globals': true,
+    'cypress/globals': true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'jest', 'cypress', 'prettier'],
+  rules: {
+    indent: ['error', 2],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
